d4ead2fc2dd560c762b398c775bc1a49485699da
Bradneal14 <bradleyhneal@gmail.com>
2016-03-28 19:15:30 -0700

    working with animation

--- object.js
+++ object.js
@@ -4,7 +4,7 @@
   }
 
   var MovingObject = Asteroids.MovingObject = function (options) {
-    this.pos = options.pos;
+    this.pos =options.pos;
     this.vel = options.vel;
     this.radius = options.radius;
     this.color = options.color;
